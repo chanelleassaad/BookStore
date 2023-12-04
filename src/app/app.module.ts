54c3d7cf65caf9a0702582cc30c8208209387883
chanelleassaad <assaadchanelle@gmail.com>
2023-12-04 16:59:58 +0200

    Add themed colors ro user login Add error messages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,14 @@ import { BookCardComponent } from './components/book-card/book-card.component';
 import { UserLoginComponent } from './components/user-login/user-login.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
 import {ToastrModule} from "ngx-toastr";
-import {ReactiveFormsModule} from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {AlertModule} from "ngx-bootstrap/alert";
 import {CommonModule} from "@angular/common";
+import {MatTabsModule} from "@angular/material/tabs";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatButtonModule} from "@angular/material/button";
+import {MatInputModule} from "@angular/material/input";
 
 @NgModule({
   declarations: [
@@ -23,15 +27,20 @@ import {CommonModule} from "@angular/common";
     UserLoginComponent,
     ContactUsComponent,
   ],
-  imports: [
-    AppRoutingModule,
-    CommonModule,
-    BrowserModule,
-    ReactiveFormsModule,
-    BrowserAnimationsModule,
-    ToastrModule.forRoot(),
-    AlertModule,
-  ],
+    imports: [
+        AppRoutingModule,
+        CommonModule,
+        BrowserModule,
+        ReactiveFormsModule,
+        BrowserAnimationsModule,
+        ToastrModule.forRoot(),
+        AlertModule,
+        MatTabsModule,
+        MatFormFieldModule,
+        MatButtonModule,
+        MatInputModule,
+        FormsModule,
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/components/user-login/user-login.component.ts
+++ src/app/components/user-login/user-login.component.ts
@@ -1,9 +1,47 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 @Component({
   selector: 'app-user-login',
   templateUrl: './user-login.component.html',
   styleUrls: ['./user-login.component.css']
 })
 export class UserLoginComponent {
+
+  loginForm: FormGroup;
+  registerForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    // Initialize form groups and controls
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+
+    this.registerForm = this.fb.group({
+      name: ['', Validators.required],
+      username: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+
+  // Accessors for form controls
+  get loginFormControls() {
+    return this.loginForm.controls;
+  }
+
+  get registerFormControls() {
+    return this.registerForm.controls;
+  }
+
+  submitLoginForm() {
+    // Implement login form submission logic here
+    console.log('Login form submitted:', this.loginForm.value);
+  }
+
+  submitRegisterForm() {
+    // Implement register form submission logic here
+    console.log('Register form submitted:', this.registerForm.value);
+  }
 
 }
