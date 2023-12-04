3069f2c87ee257dd70378242112af6d11aea3cdd
chanelleassaad <assaadchanelle@gmail.com>
2023-12-04 23:24:36 +0200

    Create modal for book details

--- src/app/components/book-card/book-card.component.ts
+++ src/app/components/book-card/book-card.component.ts
@@ -6,14 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./book-card.component.css']
 })
 export class BookCardComponent {
-  books: Array<{ src: String, bookName: String, author: String, price: number }> = [
-    {src: '/assets/images/book-cover.jpg', author: 'Colleen Hoover', bookName: 'It Ends With Us', price: 23},
-    {src: '/assets/images/book-cover2.jpg', author: 'Colleen Hoover', bookName: 'It Ends With Us', price: 23},
-    {src: '/assets/images/book-cover.jpg', author: 'Colleen Hoover', bookName: 'It Ends With Us', price: 23},
-    {src: '/assets/images/book-cover2.jpg', author: 'Colleen Hoover', bookName: 'It Ends With Us', price: 23},
-    {src: '/assets/images/book-cover2.jpg', author: 'Colleen Hoover', bookName: 'It Ends With Us', price: 23},
-    {src: '/assets/images/book-cover.jpg', author: 'Colleen Hoover', bookName: 'It Ends With Us', price: 23},
-    {src: '/assets/images/book-cover.jpg', author: 'Colleen Hoover', bookName: 'It Ends With Us', price: 23},
+  books: Array<{ src: String, bookName: String, author: String, price: number, publishedBy: string, publishYear: string, description: string, categories: Array< string>  }> = [
+    {src: '/assets/images/book-cover.jpg', author: 'Colleen Hoover', bookName: 'It Ends With Us', price: 23, publishedBy: 'X Y', publishYear: '2019', description: 'hx wwucb jwcgv wecjwuegbcv wejchb wcjwbec w cjuowec w ejcjwe', categories: ["novel","horror"]},
+    {src: '/assets/images/book-cover.jpg', author: 'Colleen Hoover', bookName: 'It Ends With Us', price: 23, publishedBy: 'X Y', publishYear: '2019', description: 'hx wwucb jwcgv wecjwuegbcv wejchb wcjwbec w cjuowec w ejcjwe', categories: ["novel","horror"]},
+    {src: '/assets/images/book-cover2.jpg', author: 'Colleen Hoover', bookName: 'It Ends With Us', price: 23, publishedBy: 'X Y', publishYear: '2019', description: 'hx wwucb jwcgv wecjwuegbcv wejchb wcjwbec w cjuowec w ejcjwe', categories: ["novel","horror"]},
+    {src: '/assets/images/book-cover2.jpg', author: 'Colleen Hoover', bookName: 'It Ends With Us', price: 23, publishedBy: 'X Y', publishYear: '2019', description: 'hx wwucb jwcgv wecjwuegbcv wejchb wcjwbec w cjuowec w ejcjwe', categories: ["novel","horror"]},
+    {src: '/assets/images/book-cover.jpg', author: 'Colleen Hoover', bookName: 'It Ends With Us', price: 23, publishedBy: 'X Y', publishYear: '2019', description: 'hx wwucb jwcgv wecjwuegbcv wejchb wcjwbec w cjuowec w ejcjwe', categories: ["novel","horror"]},
+    {src: '/assets/images/book-cover2.jpg', author: 'Colleen Hoover', bookName: 'It Ends With Us', price: 23, publishedBy: 'X Y', publishYear: '2019', description: 'hx wwucb jwcgv wecjwuegbcv wejchb wcjwbec w cjuowec w ejcjwe', categories: ["novel","horror"]},
+    {src: '/assets/images/book-cover.jpg', author: 'Colleen Hoover', bookName: 'It Ends With Us', price: 23, publishedBy: 'X Y', publishYear: '2019', description: 'hx wwucb jwcgv wecjwuegbcv wejchb wcjwbec w cjuowec w ejcjwe', categories: ["novel","horror"]},
   ];
 
+
+  selectedBook: any = null;
+
+  openDetailsModal(book: any) {
+    this.selectedBook = book;
+  }
+
+  closeDetailsModal() {
+    this.selectedBook = null;
+  }
+
+  addToCart() {
+
+  }
 }
